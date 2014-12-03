85983ac224e009d8f448a43e876315283247e7a8
Jonas Gröger <jonas.groeger@gmail.com>
2014-12-03 19:56:51 +0100

    Added buildfiles and simple CMakeLists.txt

---
+++ forwardSim.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -o errexit
+
+BUILD_FOLDER="_build"
+
+if [ ! -d "$BUILD_FOLDER" ]; then
+    echo "You must first build the project (chmod +x build.sh && ./build.sh)."
+    exit 1
+fi
+
+if [ ! -f "$1" ]; then
+	echo "Usage: ./forwardSim.sh buildlist"
+	exit 2
+fi
+
