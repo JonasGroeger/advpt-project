6cb7271a495f13ebd680892f4932042f7e52ef15
Simon Rainer <sr@mail25.de>
2014-12-09 13:50:19 +0100

    Hello Wor

---
+++ src/BuildStep.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+class BuildStep
+{
+private:
+        BuildStepType type;
+        Entity which;
+public:
+        BuildStepType getType();
+        Entity getWhich();
+}
---
+++ src/Game.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+class Game 
+{
+        void executeBuildStep(BuildStep step);
+public:
+        void loop();
+        bool isFinished();
+        GameState& getFinalState();
+}
