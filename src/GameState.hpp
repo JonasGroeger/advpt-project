c9f0b8c6f3e16b19981d9cf6187318359976f43e
Jonas Gröger <jonas.groeger@gmail.com>
2014-12-09 19:41:58 +0100

    Added EntityTypes (my fingers hrt) + the rest of gamestate.

--- src/GameState.hpp
+++ src/GameState.hpp
@@ -3,6 +3,24 @@
 class GameState
 {
 public:
-    hasEnough(
+    bool hasEnoughMinerals(long amount);
+    bool hasEnoughVespine(long amount);
+    bool hasEnoughSupply(long amount);
+    bool hasEntity(EntityType& type, long amount);
 
+    void consumeEnoughMinerals(long amount);
+    void consumeEnoughVespine(long amount);
+    void consumeEnoughSupply(long amount);
+
+    void addMinerals(long amount);
+    void addVespine(long amount);
+
+    void addEntity(EntityType& type, long amount);
+    void removeEntity(Entity& entity);
+    void changeEntity(Entity& old: Entity& theNew);
+
+    std::vector<EntityType>& getEntities(EntityType& type);
+    std::vector<Upgradables>& getUpgradeables();
+    std::vector<Updatables>& getUpdatables();
+    std::vector<Producer>& getProducers();
 };
