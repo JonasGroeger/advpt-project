2494d137e3c042a185e35dc54c468388485417fd
Simon Rainer <sr@mail25.de>
2014-12-12 15:37:57 +0100

    Fixed tabs in BuildOrder to use 4 Spaces and moved #includes to header.

--- include/BuildOrder.hpp
+++ include/BuildOrder.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <cstdio>
 #include <fstream>
 #include <sstream>
 #include <string>
--- src/BuildOrder.cpp
+++ src/BuildOrder.cpp
@@ -1,14 +1,13 @@
 #include "BuildOrder.hpp"
-#include <cstdio>
 
 BuildStep* BuildOrder::getNextStep(){
-	if(iterator != buildSteps.end()){
-		++iterator;
-		return *iterator;
-	}
-	return nullptr;
+    if(iterator != buildSteps.end()){
+        ++iterator;
+        return *iterator;
+    }
+    return nullptr;
 }
 
 void BuildOrder::advance(){
-	
+
 }
