9e0823dca086e4342f18e9d894186163afd24030
Sebastian Haubner <sebastian.haubner@fau.de>
2014-12-18 21:09:07 +0100

    resolves #14

--- include/Entity.hpp
+++ include/Entity.hpp
@@ -14,13 +14,23 @@ protected:
     unsigned char UPGRADABLE_INTERFACE = 1 << 2;
     //00001000
     unsigned char PRODUCER_INTERFACE = 1 << 3;
-    //holds all
+
+    /*
+    holds all implemented interfaces
+    has to be set in the constructor of an instance
+    for example interfaceBitmask = UPDATABLE_INTERFACE | PRODUCER_INTERFACE
+    means that this entity implements those two interfaces
+    */
     unsigned char interfaceBitmask = 0;
 
 public:
     virtual EntityType getType();
     virtual ~Entity();
 
+    /*
+    The following methods will return if a specific interface is implemented by this instance
+    addEntity inside GameState has to check if a entity is of a specific type by calling them
+     */
     bool isProducer() {
         bool hasInterface = (interfaceBitmask & PRODUCER_INTERFACE);
         return hasInterface;
