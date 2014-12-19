f8c3edb92cda0b3316233cac08a13314423adb1d
Sebastian Haubner <sebastian.haubner@fau.de>
2014-12-19 01:37:30 +0100

    add Worker Base class and header add TypeOfWork enum add them to the CMakeList

---
+++ include/TypeOfWork.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+enum TypeOfWork{
+        Idle,
+        Vespine,
+        Minerals,
+        Producing
+};
---
+++ include/entities/Worker.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include "TypeOfWork.hpp"
+
+class Worker
+{
+private:
+    //every worker is idleing at the beginning
+    TypeOfWork currentWork = TypeOfWork::Idle;
+
+public:
+    void setTypeOfWork(TypeOfWork work);
+    TypeOfWork getTypeOfWork();
+};
