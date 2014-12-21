116c75679564a5848ce4dcac6469ee96e9faadf2
Simon Rainer <sr@mail25.de>
2014-12-21 16:18:36 +0100

    Fixed missing type in command center

--- src/entities/terran/ProductionBuildings.cpp
+++ src/entities/terran/ProductionBuildings.cpp
@@ -4,6 +4,7 @@
 CommandCenter::CommandCenter()
 {
     interfaceBitmask = UPDATABLE_INTERFACE | PRODUCER_INTERFACE | UPGRADABLE_INTERFACE;
+    type = TERRAN_COMMAND_CENTER;
 }
 
 bool CommandCenter::produceEntityIfPossible(EntityType type, GameState& state)
@@ -36,13 +37,13 @@ bool CommandCenter::produceEntityIfPossible(EntityType type, GameState& state)
 // TODO
 bool CommandCenter::upgradeIfPossible(EntityType type, GameState &state)
 {
+    product = type;
     switch (type)
     {
         case TERRAN_ORBITAL_COMMAND:
             if (state.hasEnoughMinerals(150))
             {
                 this->state = UPState::UPGRADING;
-                product = type;
                 maxProgress = 35;
                 state.consumeEnoughMinerals(150);
                 return true;
@@ -51,7 +52,6 @@ bool CommandCenter::upgradeIfPossible(EntityType type, GameState &state)
         case TERRAN_PLANETARY_FORTRESS:
             if (state.hasEnough(150, 150, 0) && state.hasEntity(TERRAN_ENGINEERING_BAY))
             {
-                product = type;
                 maxProgress = 50;
                 state.consumeEnoughMinerals(150);
                 state.consumeEnoughVespine(150);
@@ -63,9 +63,6 @@ bool CommandCenter::upgradeIfPossible(EntityType type, GameState &state)
     }
     return false;
 }
-
-
-
 
 Barracks::Barracks()
 {
