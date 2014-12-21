d4d56bbf741764b1f5d4dd1879211d657edadb66
Sebastian Haubner <sebastian.haubner@fau.de>
2014-12-22 00:15:32 +0100

    changed getTimeToFinish in warphelper

--- src/entities/protoss/WarpHelper.cpp
+++ src/entities/protoss/WarpHelper.cpp
@@ -3,16 +3,13 @@
 WarpHelper* WarpHelper::_instance = 0;
 
 /* Updatable */
-void WarpHelper::update(GameState &state){
+void WarpHelper::update(GameState &state)
+{
     auto it = warpTasks.begin();
     while (it != std::end(warpTasks)) {
         // do some stuff
         if((*it)->isFinished()){
             state.addEntity((*it)->getType(), 1);
-
-            maxTime = ((*it)->getTimeToFinish() > maxTime)
-                    ? (*it)->getTimeToFinish()
-                    : maxTime;
 
             WarpTask* toDelete = *it;
             warpTasks.erase(it);
@@ -25,16 +22,25 @@ void WarpHelper::update(GameState &state){
     }
 }
 
-bool WarpHelper::produceEntityIfPossible(EntityType type, GameState& state){
+bool WarpHelper::produceEntityIfPossible(EntityType type, GameState& state)
+{
     //TODO
 }
 
-long WarpHelper::getTimeToFinish(){
+long WarpHelper::getTimeToFinish()
+{
+    for(auto task : warpTasks){
+        maxTime = (task->getTimeToFinish() > maxTime)
+                ? task->getTimeToFinish()
+                : maxTime;
+    }
+
     return maxTime;
 }
 
 
-void WarpHelper::warpBuilding(int duration, EntityType type, GameState &state){
+void WarpHelper::warpBuilding(int duration, EntityType type, GameState &state)
+{
     WarpTask* newTask = new WarpTask(duration, type);
     warpTasks.push_back(newTask);
     state.notifyEntityIsBeingProduced(type);
