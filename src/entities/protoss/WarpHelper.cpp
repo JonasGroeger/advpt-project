952163e9b36a783fd7502dc14c4799384a784547
Sebastian Haubner <sebastian.haubner@fau.de>
2014-12-22 11:30:24 +0100

    added more dependencies for protoss buildings in warphelper changed protoss3.txt a bit

--- src/entities/protoss/WarpHelper.cpp
+++ src/entities/protoss/WarpHelper.cpp
@@ -44,52 +44,85 @@ bool WarpHelper::produceEntityIfPossible(EntityType type, GameState &state)
             time = 30;
             break;
         case PROTOSS_GATEWAY:
+            if(!state.hasEntity(PROTOSS_NEXUS) || !state.hasEntity(PROTOSS_PYLON)){
+                return false;
+            }
             minerals = 150;
             time = 65;
             break;
         case PROTOSS_FORGE:
+            if(!state.hasEntity(PROTOSS_NEXUS) || !state.hasEntity(PROTOSS_PYLON)){
+                return false;
+            }
             minerals = 150;
             time = 45;
             break;
         case PROTOSS_CYBERNETICS_CORE:
+            if(!state.hasEntity(PROTOSS_GATEWAY)){
+                return false;
+            }
             minerals = 150;
             time = 50;
             break;
         case PROTOSS_PHOTON_CANNON:
+            if(!state.hasEntity(PROTOSS_FORGE)){
+                return false;
+            }
             minerals = 150;
             time = 40;
             break;
         case PROTOSS_ROBOTICS_FACILITY:
+            if(!state.hasEntity(PROTOSS_CYBERNETICS_CORE)){
+                return false;
+            }
             minerals = 200;
             gas = 100;
             time = 60;
             break;
         case PROTOSS_TWILIGHT_COUNCIL:
+            if(!state.hasEntity(PROTOSS_CYBERNETICS_CORE)){
+                return false;
+            }
             minerals = 150;
             gas = 100;
             time = 50;
             break;
         case PROTOSS_STARGATE:
+            if(!state.hasEntity(PROTOSS_CYBERNETICS_CORE)){
+                return false;
+            }
             minerals = 150;
             gas = 150;
             time = 60;
             break;
         case PROTOSS_ROBOTICS_BAY:
+            if(!state.hasEntity(PROTOSS_ROBOTICS_FACILITY)){
+                return false;
+            }
             minerals = 200;
             gas = 200;
             time = 65;
             break;
         case PROTOSS_TEMPLAR_ARCHIVES:
+            if(!state.hasEntity(PROTOSS_TWILIGHT_COUNCIL)){
+                return false;
+            }
             minerals = 150;
             gas = 200;
             time = 50;
             break;
         case PROTOSS_DARK_SHRINE:
+            if(!state.hasEntity(PROTOSS_TWILIGHT_COUNCIL)){
+                return false;
+            }
             minerals = 100;
             gas = 250;
             time = 100;
             break;
         case PROTOSS_FLEET_BEACON:
+            if(!state.hasEntity(PROTOSS_STARGATE)){
+                return false;
+            }
             minerals = 300;
             gas = 200;
             time = 60;
