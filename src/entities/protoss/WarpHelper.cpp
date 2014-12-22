d6e1923845c49186e772879a8e499fab8218b085
Sebastian Haubner <sebastian.haubner@fau.de>
2014-12-22 09:20:30 +0100

    fixed SegFault in WarpHelper -added producable buildings to warphelper

--- src/entities/protoss/WarpHelper.cpp
+++ src/entities/protoss/WarpHelper.cpp
@@ -6,7 +6,8 @@ WarpHelper* WarpHelper::_instance = 0;
 void WarpHelper::update(GameState &state)
 {
     auto it = warpTasks.begin();
-    while (it != std::end(warpTasks)) {
+    auto end = warpTasks.end();
+    while (it != end) {
         // do some stuff
         if((*it)->isFinished()){
             state.addEntity((*it)->getType(), 1);
@@ -24,7 +25,80 @@ void WarpHelper::update(GameState &state)
 
 bool WarpHelper::produceEntityIfPossible(EntityType type, GameState& state)
 {
-    //TODO
+    int minerals = 0, gas = 0, time = 0;
+    switch(type){
+        case PROTOSS_NEXUS:
+            minerals = 400;
+            time = 100;
+            break;
+        case PROTOSS_PYLON:
+            minerals = 100;
+            time = 25;
+            break;
+        case PROTOSS_ASSIMILATOR:
+            minerals = 75;
+            time = 30;
+            break;
+        case PROTOSS_GATEWAY:
+            minerals = 150;
+            time = 65;
+            break;
+        case PROTOSS_FORGE:
+            minerals = 150;
+            time = 45;
+            break;
+        case PROTOSS_CYBERNETICS_CORE:
+            minerals = 150;
+            time = 50;
+            break;
+        case PROTOSS_PHOTON_CANNON:
+            minerals = 150;
+            time = 40;
+            break;
+        case PROTOSS_ROBOTICS_FACILITY:
+            minerals = 200;
+            gas = 100;
+            time = 60;
+            break;
+        case PROTOSS_TWILIGHT_COUNCIL:
+            minerals = 150;
+            gas = 100;
+            time = 50;
+            break;
+        case PROTOSS_STARGATE:
+            minerals = 150;
+            gas = 150;
+            time = 60;
+            break;
+        case PROTOSS_ROBOTICS_BAY:
+            minerals = 200;
+            gas = 200;
+            time = 65;
+            break;
+        case PROTOSS_TEMPLAR_ARCHIVES:
+            minerals = 150;
+            gas = 200;
+            time = 50;
+            break;
+        case PROTOSS_DARK_SHRINE:
+            minerals = 100;
+            gas = 250;
+            time = 100;
+            break;
+        case PROTOSS_FLEET_BEACON:
+            minerals = 300;
+            gas = 200;
+            time = 60;
+            break;
+        default:
+            return false;
+    }
+
+    if(state.hasEnough(minerals, gas, 0)){
+        state.consumeEnough(minerals, gas, 0);
+        warpBuilding(time, type, state);
+        return true;
+    }
     return false;
 }
 
