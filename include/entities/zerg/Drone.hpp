7bc04285d3fc22d4bbd0c346aab2b821442a6768
Simon Rainer <sr@mail25.de>
2014-12-22 04:55:04 +0100

    Half-added Drone

---
+++ include/entities/zerg/Drone.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <iostream>
+
+#include "Entity.hpp"
+#include "Updatable.hpp"
+#include "GameState.hpp"
+#include "entities/Worker.hpp"
+
+class Drone : public Entity,
+              public Worker,
+              public Updatable,
+              public Upgradable
+{
+    private:
+        int currentProgress = 0;
+        int maxProgress = 0;
+        bool morphing = false;
+
+    /* class Entity */
+    public:
+        Drone();
+
+    /* class Updatable */
+    public:
+        void update(GameState &state) final;
+
+    /* class Upgradable */
+    public:
+        bool upgradeIfPossible(EntityType type, GameState &state) final;
+};
---
+++ src/entities/zerg/Drone.cpp
@@ -0,0 +1,47 @@
+#include "entities/zerg/Drone.hpp"
+
+Drone::Drone()
+{
+    interfaceBitmask = UPDATABLE_INTERFACE | WORKER_INTERFACE;
+    type = ZERG_DRONE;
+}
+
+void Drone::update(GameState &state)
+{
+    if (morphing)
+    {
+        currentProgress ++;
+        if (currentProgress >= maxProgress)
+        {
+            // TODO turn this into a spawning pool
+            std::cout << "SPAWNING POOL BUILT" << std::endl;
+        }
+        return;
+    }
+    switch (this->getTypeOfWork())
+    {
+        case TypeOfWork::Minerals:
+            state.addMineralsWithFactor(0.7 * GameState::FACTOR);
+            break;
+        case TypeOfWork::Vespine:
+            state.addVespineWithFactor(0.35 * GameState::FACTOR);
+            break;
+        case TypeOfWork::Idle:
+        case TypeOfWork::Producing:
+            break;
+    }
+}
+
+bool Drone::upgradeIfPossible(EntityType type, GameState &state)
+{
+        if (type == ZERG_SPAWNING_POOL && state.hasEnough(200, 0, 0))
+        {
+                state.consumeEnough(200, 0, 0);
+                state.notifyEntityIsBeingProduced(type);
+                currentProgress = 0;
+                maxProgress = 65;
+                morphing = true;
+                return true;
+        }
+        return false;
+}
