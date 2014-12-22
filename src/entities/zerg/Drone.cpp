22a2f17bc14f2c272fc4e54f0dfa7e70b246a6ef
Simon Rainer <sr@mail25.de>
2014-12-22 16:36:13 +0100

    Added buildings

--- src/entities/zerg/Drone.cpp
+++ src/entities/zerg/Drone.cpp
@@ -35,15 +35,71 @@ void Drone::update(GameState &state)
 
 bool Drone::upgradeIfPossible(EntityType type, GameState &state)
 {
-        if (type == ZERG_SPAWNING_POOL && state.hasEnough(200, 0, 0))
-        {
-                state.consumeEnough(200, 0, 0);
-                state.notifyEntityIsBeingProduced(type);
-                currentProgress = 0;
-                maxProgress = 65;
-                morphing = true;
-                product = type;
-                return true;
-        }
-        return false;
+    int minerals = 0, gas = 0, time = 0;
+    switch (type)
+    {
+        case ZERG_SPAWNING_POOL:
+            minerals = 200;
+            time = 65;
+            break;
+        case ZERG_SPORE_CRAWLER:
+            minerals = 75;
+            time = 30;
+            break;
+        case ZERG_HYDRALISK_DEN:
+            minerals = 100;
+            gas = 100;
+            time = 40;
+            break;
+        case ZERG_NYDUS_NETWORK:
+            minerals = 150;
+            gas  = 200;
+            time = 50;
+            break;
+        case ZERG_EVOLUTION_CHAMBER:
+            minerals = 75;
+            time = 35;
+            break;
+        case ZERG_INFESTATION_PIT:
+            minerals = 100;
+            gas = 100;
+            time = 50;
+            break;
+        case ZERG_EXTRACTOR:
+            minerals = 25;
+            time = 30;
+            break;
+        case ZERG_SPINE_CRAWLER:
+            minerals = 100;
+            time = 50;
+            break;
+        case ZERG_ROACH_WARREN:
+            minerals = 180;
+            time = 55;
+            break;
+        case ZERG_BANELING_NEST:
+            minerals = 100;
+            gas = 50;
+            time = 60;
+            break;
+        case ZERG_SPIRE:
+            minerals = 200;
+            gas = 200;
+            time = 100;
+            break;
+        default:
+            return false;
+    }
+
+    if (state.hasEnough(minerals, gas, 0))
+    {
+        state.consumeEnough(minerals, gas, 0);
+        state.notifyEntityIsBeingProduced(type);
+        currentProgress = 0;
+        maxProgress = time;
+        morphing = true;
+        product = type;
+        return true;
+    }
+    return false;
 }
