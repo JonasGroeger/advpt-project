59e1c71dbcc0c93594f863f1c7566395d0765ff2
Jonas Gröger <jonas.groeger@gmail.com>
2015-03-08 17:52:15 +0100

    Added XML loading.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <tinyxml2.h>
 
-int main(int argc, char **argv)
+class Config
 {
-    std::cout << "Hello World!" << std::endl;
+
+};
+
+int main(int argc, const char *argv[])
+{
+    if (argc < 2)
+    {
+        std::cerr << "Please provide a configuration file." << std::endl;
+        return 1;
+    }
+
+    std::string configFile = argv[1];
+
+    tinyxml2::XMLDocument doc;
+    tinyxml2::XMLError e = doc.LoadFile(configFile.c_str());
+    if (e != 0)
+    {
+        std::cerr << "Could not read configuration file. Is it really there?\n    " << configFile << std::endl;
+        return 2;
+    }
+
+    //tinyxml2::XMLElement *root = doc.RootElement();
+
+    return 0;
 }
