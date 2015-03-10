f5cc9d48a7f2e0a949fae55096b1a05f861c7c50
Sebastian Haubner <sebastian.haubner@fau.de>
2015-03-10 20:45:28 +0100

    added fields for the config values

--- include/parser.h
+++ include/parser.h
@@ -1,7 +1,14 @@
 #pragma once
 
 #include <tinyxml2.h>
+#include <iostream>
+#include <string>
+#include <map>
 using namespace tinyxml2;
+using std::cout;
+using std::endl;
+using std::string;
+using std::map;
 
 class Parser
 {
@@ -10,9 +17,33 @@ public:
 
 protected:
     bool checkForXmlError(tinyxml2::XMLError e);
+    void parseWorkers();
+    void parseGasHarvesters();
     void createAction();
 
+    void addUnitToMap(string unitName);
+
 private:
+    const char* NODE_ROOT = "sc2sim";
+    const char* NODE_MAX_UNITS = "max_units";
+    const char* NODE_WORKER = "workers";
+    const char* NODE_GAS_HARVESTER = "gas_harvesters";
+    const char* NODE_ACTION = "action";
+    const char* NODE_UNIT = "unit";
+    const char* NODE_COSTS = "costs";
+    const char* NODE_BORROWS = "borrows";
+    const char* NODE_DEPENDENCIES = "dependencies";
+    const char* NODE_RESULTS = "results";
+
+    const char* ATTRIBUTE_NAME = "name";
+    const char* ATTRIBUTE_MINERALS = "minerals";
+    const char* ATTRIBUTE_GAS = "gas";
+    const char* ATTRIBUTE_TIME = "time";
+    const char* ATTRIBUTE_SUPPLY = "supply";
+
     char* configPath;
     XMLDocument xmlConfig;
+    XMLNode* rootNode;
+    map<string, int> unitMap;
+    int unitCount = 0;
 };
